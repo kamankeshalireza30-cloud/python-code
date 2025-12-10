ccf48c788bfed1d609425c2a84e35d9f6c4cb75b
kamankeshalireza30-cloud <kamankeshalireza30@gmail.com>
2025-12-10 15:33:39 +0330

    Add Python script to calculate Euclidean distance and print values

--- exercise/code-1.py
+++ exercise/code-1.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import math
+coordinates = (4, 3)
+origin = (0,0) 
+distance = (math.sqrt ((coordinates[0] - origin[0])**2 + (coordinates[1] - origin[1])**2))
+print(distance)
+x=11
+print(x)
+print((x))
